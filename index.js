527b00edf8657104c3442d05e29cb846acb49c47
Shubham  Saurav <shubhamsinha186@gmail.com>
2022-12-02 13:26:10 +0530

    Added routers in index fikle

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 
-const journalRouter = require('./routes/journals')
+const journalsRouter = require('./routes/journals')
+const albumsRouter = require('./routes/albums')
+const todosRouter = require('./routes/todos')
+const notebooksRouter = require('./routes/notebooks')
+const notesRouter = require('./routes/notes')
+const remindersRouter = require('./routes/reminders')
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use('/api/v1/journals', journalRouter)
+app.use('/api/v1/journals', journalsRouter)
+app.use('/api/v1/albums', albumsRouter)
+app.use('/api/v1/notebooks', notebooksRouter)
+app.use('/api/v1/todos', todosRouter)
+app.use('/api/v1/notes', notesRouter)
+app.use('/api/v1/reminders', remindersRouter)
 
 app.all('*', (req, res, next) => {
     res.send(404).json({
